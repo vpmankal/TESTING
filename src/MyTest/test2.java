3995a39387e4941af7e7ce464116751265dd1f80
DivyaVMankal <DivyaVMankal@host.docker.internal>
2024-02-13 20:15:37 +0530

    committing test2 to vpmNewBranch

---
+++ src/MyTest/test2.java
@@ -0,0 +1,15 @@
+package MyTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test2 {
+	public void setup(){
+			WebDriver driver = new ChromeDriver();
+			driver.get("https://www.google.com");
+		String tt= 	driver.getTitle();
+		System.out.println(tt);
+		
+	}
+
+}
